90d4dae4335aac335a88d75e7583229040207d53
ffuszthaler <flo.fuszthaler@gmail.com>
2021-07-07 19:01:21 +0200

    fixed comments

--- lua/colorscheme-config.lua
+++ lua/colorscheme-config.lua
@@ -1,8 +1,8 @@
--- Example config in Lua
+-- tokyonight
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 vim.g.tokyonight_dark_sidebar = true
 
--- Load the colorscheme
+-- load the colorscheme
 vim.cmd [[colorscheme tokyonight]]
