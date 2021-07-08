bd9c33bd188423e88c8c1b117c24e2998657e6b8
ffuszthaler <flo.fuszthaler@gmail.com>
2021-07-08 02:10:10 +0200

    added mouse support & switched some setting to lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,18 +1,19 @@
-vim.cmd "syntax on"
-vim.cmd "filetype plugin indent on"
+vim.o.syntax = "on" -- enable syntax highlighting
+vim.cmd "filetype plugin indent on" -- enables filetype specific settings
 vim.o.number = true -- set numbered lines
 -- vim.o.relativenumber = true -- set relative number
-vim.o.clipboard = "unnamed,unnamedplus" -- Copy paste between vim and everything else
-vim.cmd "set expandtab"
-vim.cmd "set ts=2"
-vim.cmd "set sw=2"
-vim.o.fileencoding = "utf-8" -- The encoding written to file
-vim.o.splitbelow = true -- Horizontal splits will automatically be below
-vim.o.splitright = true -- Vertical splits will automatically be to the right
---vim.o.t_Co = "256" -- Support 256 colors
+vim.o.clipboard = "unnamed,unnamedplus" -- copy paste between vim and everything else
+vim.o.expandtab = true -- use spaces, not tabs
+vim.o.tabstop = 2 -- number of spaces a "tab" counts for
+vim.o.shiftwidth = 2 -- number of spaces used for auto indenting
+vim.o.fileencoding = "utf-8" -- the encoding written to file
+vim.o.splitbelow = true -- horizontal splits will automatically be below
+vim.o.splitright = true -- vertical splits will automatically be to the right
+-- vim.o.t_Co = "256" -- support 256 colors
 vim.o.termguicolors = true -- set term gui colors most terminals support this
-vim.cmd "set colorcolumn=80"
-vim.o.cursorline = true -- Enable highlighting of the current line
+vim.o.colorcolumn = "80" -- adds a line at column 80
+vim.o.cursorline = true -- enable highlighting of the current line
 vim.o.updatetime = 100 -- faster completion
-vim.o.signcolumn = "yes"
-vim.o.scrolloff = 10
+vim.o.signcolumn = "yes" -- always show signcolumn
+vim.o.scrolloff = 10 -- add a 10 line scroll offset
+vim.o.mouse = "a" -- enable mouse in neovim
