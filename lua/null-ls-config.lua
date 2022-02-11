5ad6797a5cd432eca98a130e02a67f5cd46e875f
ffuszthaler <flo.fuszthaler@gmail.com>
2022-02-11 15:34:13 +0100

    added a few settings

--- lua/null-ls-config.lua
+++ lua/null-ls-config.lua
@@ -2,7 +2,9 @@ require("null-ls").setup({
   sources = {
     require("null-ls").builtins.formatting.stylua,
     require("null-ls").builtins.diagnostics.eslint,
-    require("null-ls").builtins.formatting.prettier,
+    require("null-ls").builtins.formatting.prettier.with {
+      extra_args = { "--end-of-line auto", "--single-quote" }
+    },
     require("null-ls").builtins.completion.spell,
   },
 })
