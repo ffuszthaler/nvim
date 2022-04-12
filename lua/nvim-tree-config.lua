01c75c5a80aa66a2eca27e83a66292132ee60e28
ffuszthaler <flo.fuszthaler@gmail.com>
2022-04-12 20:31:04 +0200

    updated auto-close feature for nvim-tree

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -1,6 +1,15 @@
 require("nvim-tree").setup {
-  auto_close = true,
   view = {
     side = "right",
   },
 }
+
+-- auto close if nvim-tree is the last window
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})
